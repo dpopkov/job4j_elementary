32d216c6a18345446a8a41b8761c72e73e6273ef
Denis Popkov <pkvdenis@gmail.com>
2021-12-09 20:33:21 +0300

    4.6.1. Switch [#334070 #237692] - fix MethodLength

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -4,14 +4,30 @@ public class SwitchWeek {
     public static String nameOfDay(int day) {
         String name;
         switch (day) {
-            case 1: name = "Понедельник"; break;
-            case 2: name = "Вторник"; break;
-            case 3: name = "Среда"; break;
-            case 4: name = "Четверг"; break;
-            case 5: name = "Пятница"; break;
-            case 6: name = "Суббота"; break;
-            case 7: name = "Воскресенье"; break;
-            default: name = "Ошибка"; break;
+            case 1:
+                name = "Понедельник";
+                break;
+            case 2:
+                name = "Вторник";
+                break;
+            case 3:
+                name = "Среда";
+                break;
+            case 4:
+                name = "Четверг";
+                break;
+            case 5:
+                name = "Пятница";
+                break;
+            case 6:
+                name = "Суббота";
+                break;
+            case 7:
+                name = "Воскресенье";
+                break;
+            default:
+                name = "Ошибка";
+                break;
         }
         return name;
     }
