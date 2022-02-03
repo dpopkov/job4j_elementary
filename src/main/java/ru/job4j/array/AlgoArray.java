9b66f93223eedd7b11071d0810c3e67fe3191935
Denis Popkov <pkvdenis@gmail.com>
2022-02-03 20:54:53 +0300

    6.2. Упорядочить массив. [#4441], исправление

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -4,18 +4,18 @@ public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
 
-        swap(array, 0, 3);
-        swap(array, 1, 2);
-        swap(array, 3, 4);
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
 
-        for (int i : array) {
-            System.out.println(i);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
-    }
-
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
     }
 }
