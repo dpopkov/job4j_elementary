eb90d5fdb31b562c33ab1714d7bf972114715957
Denis Popkov <pkvdenis@gmail.com>
2022-05-02 18:38:50 +0300

    6.2.3. Перевернуть массив. [#226790] - remove local variables.

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        final int middle = array.length / 2;
-        final int last = array.length - 1;
-        for (int i = 0; i < middle; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
-            int j = last - i;
+            int j = array.length - 1 - i;
             array[i] = array[j];
             array[j] = temp;
         }
