db46f245ae0d5253cb9401b735785b0f9038b7ad
Denis Popkov <pkvdenis@gmail.com>
2022-07-04 15:10:39 +0300

    6.4.2. Обход массива с последнего элемента [#361619] - swap indexes

--- src/main/java/ru/job4j/array/RollBackArray.java
+++ src/main/java/ru/job4j/array/RollBackArray.java
@@ -4,7 +4,7 @@ public class RollBackArray {
     public static int[] rollback(int[] array) {
         int[] result = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            result[array.length - 1 - i] = array[i];
+            result[i] = array[array.length - 1 - i];
         }
         return result;
     }
