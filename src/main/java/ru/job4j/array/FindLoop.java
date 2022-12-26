4e1de6e879d4d221d3671b03d61f0e15e9611880
Denis Popkov <pkvdenis@gmail.com>
2022-12-26 12:25:33 +0300

    6.5. Поиск индекса в диапазоне. [#156318] - исправление
    замечания "В цикле необходимо идти до finish включительно".

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -14,7 +14,7 @@ public class FindLoop {
     }
 
     public static int indexInRange(int[] data, int element, int start, int finish) {
-        for (int i = start; i < finish; i++) {
+        for (int i = start; i <= finish; i++) {
             if (data[i] == element) {
                 return i;
             }
